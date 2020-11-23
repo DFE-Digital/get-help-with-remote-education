87ebb556149b50a2888a50ddda1521a4266fa06b
samprentice-dev <sam.prentice@digital.education.gov.uk>
2020-11-23 17:09:06 +0000

    converted to tile layout

--- spec/features/statuatory_obligations_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-RSpec.feature "View Statuatory Obligations Page", type: :feature do
-    scenario "Navigate to Statuatory Obligations page" do
-        visit "/statuatory_obligations.html"
-
-        expect(page).to have_text("Statuatory Obligations")
-    end
-end
