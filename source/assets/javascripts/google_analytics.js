77ae52730918ec11fb11154482540ba166c8bceb
Daniel Burnley <dan@danops.uk>
2020-11-27 11:40:44 +0000

    Add GA tracking to external links

--- source/assets/javascripts/google_analytics.js
+++ source/assets/javascripts/google_analytics.js
@@ -6,24 +6,52 @@ var analyticsID = "";
 var cookieSuffix = "";
 
 if (currentScript.hasAttribute("data-ga-id")) {
-    analyticsID = currentScript.getAttribute("data-ga-id");
-    cookieSuffix = analyticsID.substring(2, analyticsID.length);
+  analyticsID = currentScript.getAttribute("data-ga-id");
+  cookieSuffix = analyticsID.substring(2, analyticsID.length);
+}
+
+function triggerAnalyticsEvent(category, action) {
+  var options = {
+    eventCategory: category,
+    eventAction: action,
+    transport: "beacon"
+  }
+
+  ga("send", "event", options);
+};
+
+function initExternalLinkTracking() {
+  // This will attach event listeners to all links with hrefs that point to external resource.
+
+  var externalLinkSelector = 'a[href^="http"]:not([href*="' + window.location.hostname + '"])';
+
+  function trackClickEvent(event) {
+    triggerAnalyticsEvent("External Link Clicked", event.target.getAttribute("href"));
+  };
+
+  var links = document.querySelectorAll(externalLinkSelector);
+
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", trackClickEvent);
+  }
 }
 
 function initGoogleAnalytics(isDisabled) {
-    var gaDisable = 'ga-disable-' + analyticsID 
+  var gaDisable = 'ga-disable-' + analyticsID
 
-    window[gaDisable] = isDisabled;
-    window.dataLayer = window.dataLayer || [];
-    
-    function gtag() {
-      dataLayer.push(arguments);
-    }
-    
-    gtag('js', new Date());
-    gtag('config', analyticsID, {
-      'cookie_domain': document.domain
-    });
+  window[gaDisable] = isDisabled;
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+
+  gtag('js', new Date());
+  gtag('config', analyticsID, {
+    'cookie_domain': document.domain
+  });
+
+  initExternalLinkTracking();
 }
 
 function checkForAnalyticsCookies() {
@@ -33,8 +61,8 @@ function checkForAnalyticsCookies() {
 }
 
 function removeAnalyticsCookies() {
-    jsCookie.remove("_ga", { path: '/', domain: document.domain });
-    jsCookie.remove("_ga_" + cookieSuffix, { path: '/', domain: document.domain });
+  jsCookie.remove("_ga", { path: '/', domain: document.domain });
+  jsCookie.remove("_ga_" + cookieSuffix, { path: '/', domain: document.domain });
 }
 
 export { initGoogleAnalytics, removeAnalyticsCookies, checkForAnalyticsCookies }
