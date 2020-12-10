09b7c1e10704722ba155a7ed284533245bbeace4
Sam Prentice <sam.prentice@digital.education.gov.uk>
2020-12-10 23:10:50 +0000

    added additional unit tests

--- ghre-rails/spec/controllers/pages_controller_spec.rb
+++ ghre-rails/spec/controllers/pages_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe PagesController, type: :controller do
   describe "GET index" do
     it "renders date_badge partial 5 times" do
       get :index
-      expect(response).to render_template(partial: '_date_badge', count: 5)
+      expect(response).to render_template(partial: '_date_badge', count: 3)
     end
   end
 end
