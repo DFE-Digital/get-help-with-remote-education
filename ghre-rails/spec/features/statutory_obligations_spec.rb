5343453e58517a52968397a935f13a8c185fc495
Sam Prentice <sam.prentice@digital.education.gov.uk>
2020-12-22 18:51:23 +0000

    remove unused let

--- ghre-rails/spec/features/statutory_obligations_spec.rb
+++ ghre-rails/spec/features/statutory_obligations_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'pages/statutory_obligations.html.erb', type: :feature do
-  let(:show_hide_changes) { page.find("[data-qa='show-hide-updates']")}
-
   describe 'update summary badge' do
     it 'should visible for up to 2 weeks' do
       allow(Date).to receive(:today).and_return I18n.t('statutory_obligations.schools.last_update').to_date + 2.weeks
