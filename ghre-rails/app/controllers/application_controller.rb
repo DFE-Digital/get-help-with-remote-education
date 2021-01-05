d1677f081be16f29c7b29f46a2cd159fef8d4725
Sam Prentice <sam.prentice@digital.education.gov.uk>
2021-01-05 16:16:55 +0000

    add govuk formbuilder

--- ghre-rails/app/controllers/application_controller.rb
+++ ghre-rails/app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   http_basic_authenticate_with name: ENV["BASIC_AUTH_USER"], password: ENV["BASIC_AUTH_PASSWORD"] if ENV["BASIC_AUTH_USER"] && ENV["BASIC_AUTH_PASSWORD"]
+  default_form_builder GOVUKDesignSystemFormBuilder::FormBuilder
 end
