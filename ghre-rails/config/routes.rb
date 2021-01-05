987151138a6ad05407a45b8c191c21c942944956
Sam Prentice <sam.prentice@digital.education.gov.uk>
2021-01-05 13:50:32 +0000

    reorder framework route

--- ghre-rails/config/routes.rb
+++ ghre-rails/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root "pages#index"
 
-  get "/404", to: "errors#not_found", via: :all
-  get "/422", to: "errors#unprocessable_entity", via: :all
-  get "/500", to: "errors#internal_server_error", via: :all
+  get "/review-framework", to: "review_framework#index"
 
   # redirects
   get "/review-your-remote-education-provision", to: redirect("https://www.gov.uk/government/publications/review-your-remote-education-provision?utm_source=remote_education")
@@ -14,8 +12,6 @@ Rails.application.routes.draw do
 
   get "/settings/cookie-preferences", to: "settings#cookie_preferences"
   post "/settings/cookie-preferences", to: "settings#update_cookie_preferences"
-
-  get "/review-framework", to: "review_framework#index"
 
   get "/404", to: "errors#not_found", via: :all
   get "/422", to: "errors#unprocessable_entity", via: :all
