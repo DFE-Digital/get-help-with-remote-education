89582dbe1318e34870e48b65599b353ab36b56aa
Sam Prentice <sam.prentice@digital.education.gov.uk>
2021-01-07 12:29:14 +0000

    add content

--- ghre-rails/spec/features/pages/send_spec.rb
+++ ghre-rails/spec/features/pages/send_spec.rb
@@ -3,13 +3,13 @@ require "rails_helper"
 RSpec.describe "/pages/send.html.erb", type: :feature do
   let(:date_updated) { I18n.t('send.training_in_assistive_technology.last_update').to_date }
 
-  before do
-    visit pages_path(page: "send")
-  end
-
   describe "Training in assistive technology section" do
+    before do
+      visit pages_path(page: "send")
+    end
+
     it "has a title" do
-      
+      expect(page).to have_selector('[data-qa="training-assistive-technology"]')
     end
   end
 end
