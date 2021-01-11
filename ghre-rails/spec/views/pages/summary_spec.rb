ded9738aeb01779b85f9ecfe82cad4f7467779ee
Sam Prentice <sam.prentice@digital.education.gov.uk>
2021-01-11 10:55:02 +0000

    use has_selector?

--- ghre-rails/spec/views/pages/summary_spec.rb
+++ ghre-rails/spec/views/pages/summary_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe "pages/summary.html.erb", type: :view do
       let(:html) { render layout: 'pages/summary', locals: { update_date: update_date, show_updates: true, page: 'send', title: 'training-in-assistive-technology', has_content: true } do; end }
 
       it "displays update details" do
-        expect(html).not_to match /data-empty/
+        expect(html).not_to have_selector('[data-empty]')
         expect(html).to have_no_css('.hidden')
       end
     end
@@ -19,7 +19,7 @@ RSpec.describe "pages/summary.html.erb", type: :view do
       let(:html) { render layout: 'pages/summary', locals: { update_date: update_date, show_updates: true, page: 'send', title: 'training-in-assistive-technology', has_content: false } do; end }
 
       it "does not display update details" do
-        expect(html).to match /data-empty/
+        expect(html).to have_selector('[data-empty]')
         expect(html).to have_css('.hidden')
       end
     end
