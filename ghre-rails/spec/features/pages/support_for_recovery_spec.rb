8a7025b58f30760a53a0699e20b9cee896e8c846
Sam Prentice <sam.prentice@digital.education.gov.uk>
2021-01-19 14:03:46 +0000

    update test

--- ghre-rails/spec/features/pages/support_for_recovery_spec.rb
+++ ghre-rails/spec/features/pages/support_for_recovery_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "page/support_for_recover.html.erb", type: :feature do
     it "links to school-to-school programme" do
       visit pages_path(page: "support-for-recovery")
 
-      expect(page).to have_selector('a[href="https://www.gov.uk/government/publications/school-leadership-support/school-leadership-support"]',
+      expect(page).to have_selector('a[href="https://www.gov.uk/government/publications/school-leadership-support/school-leadership-support?utm_source=remote_education"]',
                                     text: 'school-to-school support programme')
     end
   end
