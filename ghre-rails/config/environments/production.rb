a6f056a1e88b93d47d12c5a239b6dd19f605cd70
Sam Prentice <sam.prentice@digital.education.gov.uk>
2021-01-20 11:56:34 +0000

    add caching

--- ghre-rails/config/environments/production.rb
+++ ghre-rails/config/environments/production.rb
@@ -14,6 +14,15 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # This will affect assets served from /app/assets
+  config.static_cache_control = 'public, max-age=3600'
+
+  # This will affect assets in /public, e.g. webpacker assets.
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, max-age=3600',
+    'Expires' => 1.hour.from_now.to_formatted_s(:rfc822)
+  }
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
