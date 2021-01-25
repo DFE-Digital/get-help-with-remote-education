565fbc37460832926363ebf4ec04ee6d3ec3b781
Daniel Burnley <dan@danops.uk>
2021-01-25 14:15:47 +0000

    Reorder routes

--- ghre-rails/config/routes.rb
+++ ghre-rails/config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   get "/get-help-with-technology", to: redirect("https://get-help-with-tech.education.gov.uk/")
   get "/rapid-testing", to: redirect("https://www.gov.uk/guidance/asymptomatic-testing-in-schools-and-colleges")
 
-  get "/:page", to: "pages#show", as: :pages
 
   get "/settings/cookie-preferences", to: "settings#cookie_preferences"
   post "/settings/cookie-preferences", to: "settings#update_cookie_preferences"
@@ -23,4 +22,6 @@ Rails.application.routes.draw do
   get "/404", to: "errors#not_found", via: :all
   get "/422", to: "errors#unprocessable_entity", via: :all
   get "/500", to: "errors#internal_server_error", via: :all
+
+  get "/:page", to: "pages#show", as: :pages
 end
