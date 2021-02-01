8da724b6f1b42bf76d69e57f0c46b9396d026547
Sam Prentice <sam.prentice@digital.education.gov.uk>
2021-02-01 13:42:53 +0000

    add ryrep redirect

--- ghre-rails/config/routes.rb
+++ ghre-rails/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/500", to: "errors#internal_server_error", via: :all
 
   # redirects
+  get "/review-your-remote-education-provision", to: redirect('https://www.gov.uk/government/publications/review-your-remote-education-provision?utm_source=remote_education')
   get "/get-help-with-tech", to: redirect('https://get-help-with-tech.education.gov.uk/')
   get "/rapid-testing", to: redirect('https://www.gov.uk/guidance/asymptomatic-testing-in-schools-and-colleges')
 
