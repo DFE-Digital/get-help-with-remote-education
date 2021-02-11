5cf8943abd2acfcf0cd79729823b833cf1a5e1cf
Sam Prentice <sam.prentice@digital.education.gov.uk>
2021-02-11 14:59:53 +0000

    add updated links

--- ghre-rails/app/helpers/view_helper.rb
+++ ghre-rails/app/helpers/view_helper.rb
@@ -3,7 +3,7 @@ module ViewHelper
     link_to body, url + "?utm_source=remote_education", html_options
   end
 
-  def govuk_link_to(body, url = body, html_options = { class: "govuk-link"})
+  def govuk_link_to(body, url = body, html_options = { class: "govuk-link" })
     link_to body, url, html_options
   end
 end
