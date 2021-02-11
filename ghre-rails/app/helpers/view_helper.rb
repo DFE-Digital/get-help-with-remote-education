790879e1d49606c111aef789461bfc1c1e72e1dd
Sam Prentice <sam.prentice@digital.education.gov.uk>
2021-02-11 14:35:31 +0000

    update safeguarding content

--- ghre-rails/app/helpers/view_helper.rb
+++ ghre-rails/app/helpers/view_helper.rb
@@ -2,4 +2,8 @@ module ViewHelper
   def link_to_govuk_content(body, url = body, html_options = { class: "govuk-link" })
     link_to body, url + "?utm_source=remote_education", html_options
   end
+
+  def govuk_link_to(body, url = body, html_options = { class: "govuk-link"})
+    link_to body, url, html_options
+  end
 end
--- ghre-rails/spec/helpers/view_helper_spec.rb
+++ ghre-rails/spec/helpers/view_helper_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe ViewHelper, type: :helper do
       expect(helper.link_to_govuk_content("TEST LINK", "https://localhost:3000/")).to eq('<a class="govuk-link" href="https://localhost:3000/?utm_source=remote_education">TEST LINK</a>')
     end
   end
+
+  describe "#govuk_link_to" do
+    it "returns an anchor tag with teh content url and govuk-link styling" do
+      expect(helper.govuk_link_to("TEST", "https://www.example.com/")).to eq('<a class="govuk-link" href="https://www.example.com/">TEST</a>')
+    end
+  end
 end
