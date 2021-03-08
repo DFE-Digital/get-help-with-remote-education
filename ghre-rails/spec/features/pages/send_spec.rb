beee5a03b0a04fe702ccd8739530b25f29cc1d1c
Jedd Fenner <jedd@outlook.com>
2021-03-08 14:07:34 +0000

    mar 8th updates - final tweaks, update tests

--- ghre-rails/spec/features/pages/send_spec.rb
+++ ghre-rails/spec/features/pages/send_spec.rb
@@ -1,20 +1,10 @@
 require "rails_helper"
 
 RSpec.describe "/pages/send.html.erb", type: :feature do
-  let(:date_updated) { I18n.t("send.training_in_assistive_technology.last_update").to_date }
-
-  describe "Training in assistive technology section" do
-    before do
-      visit pages_path(page: "send")
-    end
-
-    it "has a title" do
-      expect(page).to have_selector('[data-qa="training-assistive-technology"]')
-    end
-  end
+  let(:date_updated) { I18n.t("send.last_update_date").to_date }
 
   describe "Update summary" do
-    let(:date_updated) { I18n.t("send.training_in_assistive_technology.last_update").to_date }
+    let(:date_updated) { I18n.t("send.last_update_date").to_date }
 
     it "should have one update badge" do
       Timecop.freeze(date_updated + 5.days) do
