9405f0669573dd4855579509adad554aba258978
Jedd Fenner <jedd@outlook.com>
2021-03-18 14:42:49 +0000

    add anchor option for gov_uk link helper, update guidance doc links from pdf to anchored html

--- ghre-rails/app/helpers/view_helper.rb
+++ ghre-rails/app/helpers/view_helper.rb
@@ -1,6 +1,6 @@
 module ViewHelper
-  def link_to_govuk_content(body, url = body, html_options = { class: "govuk-link" })
-    link_to body, url + "?utm_source=remote_education", html_options
+  def link_to_govuk_content(body, url = body, anchor = "", html_options = { class: "govuk-link" })
+    link_to body, url + "?utm_source=remote_education" + anchor, html_options
   end
 
   def govuk_link_to(body, url = body, html_options = { class: "govuk-link" })
