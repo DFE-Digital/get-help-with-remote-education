3e5bc9c1bc03302436476581611af5caa5327155
Jedd Fenner <jedd@outlook.com>
2021-03-30 15:05:12 +0100

    new landing page card for videos... page

--- ghre-rails/spec/features/index_spec.rb
+++ ghre-rails/spec/features/index_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 
 RSpec.describe "Home page", type: :feature do
   describe "GET index" do
-    it "renders date_badge partial 5 times" do
+    it "renders date_badge partial 6 times" do
       visit root_path
-      expect(page.find_all("[data-qa='date-badge']").count).to eq(5)
+      expect(page.find_all("[data-qa='date-badge']").count).to eq(6)
     end
 
     describe "review framework banner" do
