1ac339911b37b93ffb256885b500e08f9773aba7
Jedd Fenner <jedd.fenner@digital.education.gov.uk>
2021-03-31 16:44:38 +0100

    Design Update & New Resources (#132)
    * New landing page layout
    
    * bump update date to show updated badge
    
    * single width review...
    
    * fix tests
    
    * fix SEND card title
    
    * fix header text colour
    
    * good teaching fe resources subject detail expander
    
    * new landing page card for videos... page
    
    * split resources into levels
    
    * new fe resource links
    
    * videos page content, update badges
    
    * new fareham college resources & funky link fixes
    
    * add edtech demonstrator webinars to videos... page
    
    * typo fix
    
    * fe resources: add missing numeracy link
    
    * accordion the subjects on new videos... page
    
    * fix tense of webinar details

--- ghre-rails/spec/features/index_spec.rb
+++ ghre-rails/spec/features/index_spec.rb
@@ -2,14 +2,9 @@ require "rails_helper"
 
 RSpec.describe "Home page", type: :feature do
   describe "GET index" do
-    it "renders date_badge partial 4 times" do
+    it "renders date_badge partial 6 times" do
       visit root_path
-      expect(page.find_all("[data-qa='date-badge']").count).to eq(4)
-    end
-
-    it "has a banner linking to the static review framework" do
-      visit root_path
-      expect(page).to have_selector('[data-qa="review-framework-banner"]')
+      expect(page.find_all("[data-qa='date-badge']").count).to eq(6)
     end
 
     describe "review framework banner" do
