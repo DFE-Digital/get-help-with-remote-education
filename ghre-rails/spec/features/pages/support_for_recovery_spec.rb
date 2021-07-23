2fae310c420f93ed87ee4b8f624a4e5422444f8f
Jedd <jedd.fenner@nimbleapproach.com>
2021-07-23 14:54:01 +0100

    rewrite of, and new title for support page

--- ghre-rails/spec/features/pages/support_for_recovery_spec.rb
+++ ghre-rails/spec/features/pages/support_for_recovery_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 RSpec.describe "page/support_for_recovery.html.erb", type: :feature do
   describe "GET pages/support-for-recovery" do
-    it "links to school-to-school programme" do
+    it "links to recovery information" do
       visit pages_path(page: "support-for-recovery")
 
-      expect(page).to have_selector('a[href="https://www.gov.uk/government/publications/school-leadership-support/school-leadership-support?utm_source=remote_education"]',
-                                    text: "school-to-school support programme")
+      expect(page).to have_selector('a[href="https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/993053/Education_recovery_support_June-2021.pdf?utm_source=remote_education"]',
+                                    text: "information for early years settings, schools and providers of 16-19 education")
     end
   end
 end
