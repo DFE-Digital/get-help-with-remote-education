5f305dfcfd380349288f7bc4263bc5c7c938ad85
jonathankerr <jonathan.kerr@education.gov.uk>
2021-12-23 12:02:04 +0000

    Enable live reloading
    Live reloading, or hot reloading, means changes made to the content are immediately viewable when you run the service locally. This speeds up development
    
    Signed-off-by: jonathankerr <jonathan.kerr@education.gov.uk>

--- ghre-rails/config/environments/development.rb
+++ ghre-rails/config/environments/development.rb
@@ -48,7 +48,8 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # Use a file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. Usually this uses EventedFileUpdateChecker,
+  # but that doesn't play nicely with Docker
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 end
